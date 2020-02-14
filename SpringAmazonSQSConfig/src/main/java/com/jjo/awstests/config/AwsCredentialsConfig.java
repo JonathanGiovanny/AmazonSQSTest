2bdd32edfe8a011476c293c8aab9873a1996e8d4
Jonathan Camargo <jongiocam@gmail.com>
2020-02-13 22:32:10 -0500

    [Init] Fix app init

--- SpringAmazonSQSConfig/src/main/java/com/jjo/awstests/config/AwsCredentialsConfig.java
+++ SpringAmazonSQSConfig/src/main/java/com/jjo/awstests/config/AwsCredentialsConfig.java
@@ -1,15 +1,16 @@
 package com.jjo.awstests.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.core.region.RegionProvider;
-import org.springframework.cloud.aws.core.region.StaticRegionProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.regions.AwsEnvVarOverrideRegionProvider;
+import com.amazonaws.regions.AwsProfileRegionProvider;
+import com.amazonaws.regions.AwsRegionProvider;
 
 @Configuration
 public class AwsCredentialsConfig {
@@ -17,14 +18,18 @@ public class AwsCredentialsConfig {
   @Value("${aws.profile:default}")
   private String profileName;
 
+  private static final String PROFILE = "profile ";
+  
   @Bean
+  @Primary
   @Profile(value = {"dev"})
   public AWSCredentialsProvider devCredentialsProvider() {
     AWSCredentialsProvider credProv = new ProfileCredentialsProvider(profileName);
     return credProv;
   }
-  
+
   @Bean
+  @Primary
   @Profile(value = {"prod"})
   public AWSCredentialsProvider prodCredentialsProvider() {
     return new EnvironmentVariableCredentialsProvider();
@@ -32,7 +37,13 @@ public class AwsCredentialsConfig {
 
   @Bean
   @Profile(value = {"dev"})
-  public RegionProvider regionProvider() {
-    return new StaticRegionProvider(Regions.US_EAST_1.getName());
+  public AwsRegionProvider devRegionProvider() {
+    return new AwsProfileRegionProvider(PROFILE + profileName);
+  }
+
+  @Bean
+  @Profile(value = {"prod"})
+  public AwsRegionProvider prodRegionProvider() {
+    return new AwsEnvVarOverrideRegionProvider();
   }
 }
--- SpringAmazonSQSConfig/src/main/java/com/jjo/awstests/config/SQSConfig.java
+++ SpringAmazonSQSConfig/src/main/java/com/jjo/awstests/config/SQSConfig.java
@@ -1,20 +1,37 @@
 package com.jjo.awstests.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.regions.AwsRegionProvider;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
 @EnableSqs
 @Configuration
 public class SQSConfig {
 
-  @Bean("queueMessagingTemplate")
-  public QueueMessagingTemplate customQueue(AmazonSQSAsync amazonSqs, @Value("${aws.sqs.producer}") String queueName) {
-//    QueueMessagingTemplate messageTemplate = new QueueMessagingTemplate(amazonSqs);
-//    messageTemplate.setDefaultDestinationName(queueName);
-    return null;
+  @Autowired
+  private AWSCredentialsProvider awsCredentialsProvider;
+
+  @Autowired
+  private AwsRegionProvider awsRegionProvider;
+
+  private AmazonSQSAsync amazonSQSAsync() {
+    return AmazonSQSAsyncClientBuilder.standard()
+        .withCredentials(awsCredentialsProvider)
+        .withRegion(awsRegionProvider.getRegion())
+        .build();
+  }
+
+  @Bean
+  public QueueMessagingTemplate customQueue(@Value("${aws.sqs.producer}") String queueName) {
+    QueueMessagingTemplate messageTemplate = new QueueMessagingTemplate(amazonSQSAsync());
+    messageTemplate.setDefaultDestinationName(queueName);
+    return messageTemplate;
   }
 }
