689f51d1fea75ab3ba311b911b3f9429f61d62ea
Jonathan Camargo <jongiocam@gmail.com>
2020-02-18 22:50:36 -0500

    [Initial] Add explicit Message converter

--- SpringAmazonSQSConfig/src/main/java/com/jjo/awstests/config/SQSConfig.java
+++ SpringAmazonSQSConfig/src/main/java/com/jjo/awstests/config/SQSConfig.java
@@ -1,10 +1,12 @@
 package com.jjo.awstests.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.core.env.ResourceIdResolver;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.regions.AwsRegionProvider;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
@@ -20,6 +22,7 @@ public class SQSConfig {
 
   private @NonNull AWSCredentialsProvider awsCredentialsProvider;
   private @NonNull AwsRegionProvider awsRegionProvider;
+  private @NonNull ObjectMapper objectMapper;
 
   private AmazonSQSAsync amazonSQSAsync() {
     return AmazonSQSAsyncClientBuilder.standard()
@@ -30,13 +33,16 @@ public class SQSConfig {
 
   @Bean(name = {"queueMessagingTemplate", "bidQueueMessaging"})
   public QueueMessagingTemplate customQueue(@Value("${aws.sqs.queue-bid}") String queueName) {
-    QueueMessagingTemplate messageTemplate = new QueueMessagingTemplate(amazonSQSAsync());
+    QueueMessagingTemplate messageTemplate = new QueueMessagingTemplate(amazonSQSAsync(), (ResourceIdResolver) null, messageConverter());
     messageTemplate.setDefaultDestinationName(queueName);
     return messageTemplate;
   }
 
-  @Bean
-  public ObjectMapper buildJacksonObjectMapper() {
-      return new ObjectMapper();
+  private MappingJackson2MessageConverter messageConverter() {
+    MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+    messageConverter.setStrictContentTypeMatch(false);
+    messageConverter.setObjectMapper(objectMapper);
+    messageConverter.setSerializedPayloadClass(String.class);
+    return messageConverter;
   }
 }
